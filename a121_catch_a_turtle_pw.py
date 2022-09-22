7ea7291b3ce1c2166793daf9e9292937c024492a
WhizKid3D <55123687+WhizKid3D@users.noreply.github.com>
2022-09-22 14:32:29 -0700

    Add files via upload

--- a121_catch_a_turtle_pw.py
+++ a121_catch_a_turtle_pw.py
@@ -6,12 +6,15 @@ import random as rand
 fill_color = ("green")
 size = 2
 shape = ("turtle")
+score = 0
+font_setup = ("Arial", 20, "normal")
 #-----initialize turtle-----
 turtle = trtl.Turtle()
 turtle.shape(shape)
 turtle.shapesize(size)
 turtle.fillcolor(fill_color)
 turtle.penup()
+score_writer = trtl.Turtle()
 #-----game functions--------
 def change_position():
   (new_xpos) = rand.randint(-200,200)
@@ -19,10 +22,21 @@ def change_position():
   turtle.goto(new_xpos,new_ypos)
 
 def turtle_clicked(x,y):
+  update_score()
   change_position()
+
+def update_score():
+  global score
+  score = score + 1
+  score_writer.write(score, font=font_setup)
+
+def position_score_writer():
+  score_writer.penup()
+  score_writer.goto(200,200)
+
 #-----events----------------
 turtle.onclick(turtle_clicked)
-
+position_score_writer()
 
 wn = trtl.Screen()
 wn.mainloop()
